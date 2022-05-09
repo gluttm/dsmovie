d91e04f16906bff0a9eb6fa83e011eb5f241f96c
gluttm <valentyalberto@gmail.com>
2022-05-10 00:04:37 +0200

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
             <a href="https://github.com/gluttm" target="_blank" rel="noreferrer">
                 <div className="dsmovie-contact-container">
                     <GithubIcon />
-                    <p className="dsmovie-contact-link">/devsuperior</p>
+                    <p className="dsmovie-contact-link">/truetechmoz</p>
                 </div>
             </a>
         </div>
